534e795669591fd7c017cb649785917dafdf2864
twala <221361936@mycput.ac.za>
2023-09-10 22:28:58 +0200

    added the login and register pages

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,28 +1,50 @@
 "use client"
+import { error } from 'console';
 import Link from 'next/link';
-import React,{useState} from 'react'
+import React,{useState,FormEvent} from 'react'
+import { couldStartTrivia } from 'typescript';
 
 export default function Register() {
-  const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
+  const [surname,setSurname]=useState('');
   const [email, setEmail] = useState('');
+  const[phone,setPhone]=useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e: { preventDefault: () => void; }) => {
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+   const user = await fetch("http://localhost:8084/user/create",{headers: {
 
-    console.log(`Username: ${username}, Email: ${email}, Password: ${password}`);
+   "Content-type": "application/json; charset=UTF-8",
+
+ },method:"POST",body:JSON.stringify({
+   name,surname,email,phone
+   })}).then(v=>v.json()).catch(err=>console.log(err));
+  //  const data= await user.json()
+   console.log (user);
+    console.log(`name: ${name},surname: ${surname},Email: ${email},phone:${phone} Password: ${password}`);
   };
   return (
     <div  className="w-1/2">
       <h2 className="text-center font-bold text-purple-600">Register</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="username"className="m-3">Username:</label>
+          <label htmlFor="username"className="m-3">Name:</label>
           <input className="m-3 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
             type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="surname"className="m-3">Surname:</label>
+          <input className="m-3 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            type="text"
+            id="surname"
+            value={surname}
+            onChange={(e) => setSurname(e.target.value)}
             required
           />
         </div>
@@ -33,6 +55,16 @@ export default function Register() {
             id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="phone"className="m-3">phone:</label>
+          <input className="m-3 bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+            type="text"
+            id="phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
             required
           />
         </div>

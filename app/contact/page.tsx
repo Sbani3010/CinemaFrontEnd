9242c38ddb047b64effc1928b9bb3bdaa4dbf9ab
Luphiwe <sluphiwe@gmail.com>
2023-10-20 20:17:34 +0200

    contact page created

---
+++ app/contact/page.tsx
@@ -0,0 +1,14 @@
+import NavBar from "@/components/NavBar";
+import Contact from "@/components/home/Contact";
+import React from "react";
+
+const Page = () => {
+  return (
+    <>
+      <NavBar />
+      <Contact/>
+    </>
+  );
+};
+
+export default Page;
